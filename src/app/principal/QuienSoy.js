df89739d80ce63cf42da0eec8495be29737ddad2
frandd07 <fran.doblado.diaz@gmail.com>
2025-02-03 11:54:09 +0100

    Update

--- src/app/principal/QuienSoy.js
+++ src/app/principal/QuienSoy.js
@@ -8,7 +8,12 @@ export default function QuienSoy() {
         className="container py-5 px-4 d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}
       >
-        <div className="row align-items-center shadow-lg rounded-4 overflow-hidden bg-white">
+        <div
+          className="row align-items-center shadow-lg rounded-4 overflow-hidden "
+          style={{
+            backgroundColor: "#585953",
+          }}
+        >
           <div className="col-md-6 p-0">
             <img
               src="/images/quiensoy2.jpg"
@@ -17,14 +22,19 @@ export default function QuienSoy() {
               style={{ objectFit: "cover" }}
             />
           </div>
-          <div className="col-md-6 p-5">
+          <div
+            className="col-md-6 p-5"
+            style={{
+              backgroundColor: "#585953",
+            }}
+          >
             <span className="badge bg-warning text-dark mb-3 px-3 py-2">
               Sobre mí
             </span>
-            <h2 className="display-5 fw-bold text-dark mb-4">
+            <h2 className="display-5 fw-bold text-dark mb-4 text-white">
               Mi misión es ayudarte a alcanzar tu mejor versión
             </h2>
-            <p className="fs-5 text-muted mb-4">
+            <p className="p-4 rounded-lg text-white">
               Soy Marialu, entrenadora personal apasionada por transformar vidas
               a través de la salud y el bienestar. Mi objetivo es inspirarte y
               acompañarte en cada paso hacia una vida más activa y equilibrada.
