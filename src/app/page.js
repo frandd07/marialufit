c01e47869b7f6717d0adc2898eba46f290f3cb39
frandd07 <fran.doblado.diaz@gmail.com>
2025-03-03 10:16:16 +0100

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const route = useRouter();
+  route.push("/principal/page.js");
   return (
     <div className={styles.page}>
       <main className={styles.main}>
